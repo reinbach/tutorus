1556617126f57d61d397da9f82b3f83b748c52be
John Costa <john.costa@gmail.com>
2012-08-18 14:21:48 -0400

    add migration dependencies

--- tutorus/scratchpad/migrations/0002_auto__add_field_scratchpad_classroom.py
+++ tutorus/scratchpad/migrations/0002_auto__add_field_scratchpad_classroom.py
@@ -8,7 +8,7 @@ from django.db import models
 class Migration(SchemaMigration):
 
     depends_on = (("classroom", "0001_initial"),)
-    
+
     def forwards(self, orm):
         # Adding field 'Scratchpad.classroom'
         db.add_column('scratchpad_scratchpad', 'classroom',
