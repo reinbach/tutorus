e63f414bd1ebef9c172afdaaf7664ff69f85c57f
Greg Reinbach <greg@reinbach.com>
2012-08-18 07:20:49 -0400

    added take class framework

--- tutorus/classroom/urls.py
+++ tutorus/classroom/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns(
     url(r'^create/$', views.create_classroom, name='class_create'),
     url(r'^(?P<classroom_id>\w+)/$', views.class_create_step, name='class_create_step'),
     url(r'^(?P<classroom_id>\w+)/activate/$', views.class_activate, name='class_activate'),
+    url(r'^(?P<classroom_id>\w+)/take/$', views.class_take, name='class_take'),
     url(r'^$', views.home, name='class_home'),
 )
