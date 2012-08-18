0a8f63b46588e2b373c626e95e1d02269901c785
Greg Reinbach <greg@reinbach.com>
2012-08-17 20:32:52 -0400

    Added core app and bootstrap for styling

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ tutorus
 =======
 
 My entry for the Django Dash 2012 competition
+
+Setup/Installation
+==================
+
+pip install -r requirements.conf
--- tutorus/urls.py
+++ tutorus/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tutorus.views.home', name='home'),
-    # url(r'^tutorus/', include('tutorus.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'core.views.home', name='home'),
 )
+
+if settings.DEBUG:
+    # conditionally serve static files
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
+
