8593bc5b862302b8c5523a78e3cf0ae8e917ec7d
Greg Reinbach <greg@reinbach.com>
2012-08-18 07:19:14 -0400

    added tutor_only decorator and take class view

--- tutorus/classroom/views.py
+++ tutorus/classroom/views.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
@@ -9,6 +10,15 @@ from .models import ClassRoom
 from .forms import ClassRoomForm
 
 log = logging.getLogger(__name__)
+
+def tutor_only(func):
+    def decorator(request, classroom_id):
+        classroom = ClassRoom.objects.get(pk=classroom_id)
+        if classroom.is_tutor(request.user):
+            return func(request, classroom)
+        messages.info(request, "You are not the Tutor")
+        return HttpResponseRedirect(reverse('class_home'))
+    return decorator
 
 @login_required
 def create_classroom(request):
@@ -32,20 +42,31 @@ def create_classroom(request):
     context = {'form':form}
     return render(request, 'classroom/create.html', context)
 
-#TODO limit access to classroom to only tutor
-def class_create_step(request, classroom_id):
-    classroom = ClassRoom.objects.get(pk=classroom_id)
+@login_required
+@tutor_only
+def class_create_step(request, classroom):
     context = {'classroom': classroom}
     return render(request, 'classroom/steps.html', context)
 
-def class_activate(request, classroom_id):
-    classroom = ClassRoom.objects.get(pk=classroom_id)
-    #test
-    log.info(classroom.status)
+@login_required
+@tutor_only
+def class_activate(request, classroom):
     classroom.status = 'active'
     classroom.save()
     context = {'classroom': classroom}
     return render(request, 'classroom/steps.html', context)
+
+@login_required
+@tutor_only
+def class_take(request, classroom):
+    """Take the class
+
+    Different experience based on whether tutor or student
+    """
+    context = dict()
+    if classroom.is_tutor(request.user):
+        context.update(tutor=True)
+    return render(request, "classroom/take.html", context)
 
 def home(request):
     context = dict(
