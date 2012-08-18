b8896cd2bb776bcdc416a88735fa12b2d91c1cc1
Greg Reinbach <greg@reinbach.com>
2012-08-18 17:18:00 -0400

    changed status url to s3

--- tutorus/settings/prod.py
+++ tutorus/settings/prod.py
@@ -3,3 +3,5 @@ import dj_database_url
 from base import *
 
 DATABASES = {'default': dj_database_url.config(default='postgres://cynxhwmdptcgih:1Rhc-q2Wy7kpnDuvbJQNn8j6gY@ec2-107-20-224-107.compute-1.amazonaws.com/d6cjeo6acbuta9')}
+
+STATIC_URL = "http://s3.amazonaws.com/tutorus/"
