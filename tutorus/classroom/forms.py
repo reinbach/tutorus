14325cb53fde566181577adbbcee3ce339b8c715
Greg Reinbach <greg@reinbach.com>
2012-08-19 16:01:08 -0400

    removed obsolete code

--- tutorus/classroom/forms.py
+++ tutorus/classroom/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm, CharField
-from .models import ClassRoom
 
+from .models import ClassRoom
 
 class ClassRoomForm(ModelForm):
     name = CharField(required=True)
@@ -13,10 +13,6 @@ class ClassRoomForm(ModelForm):
 
         classroom = super(ClassRoomForm, self).save(commit=False)
         classroom.tutor = user
-        # TODO: the default status for a classroom is draft,
-        #   we should be able to remove this.
-#        if not classroom.status:
-#            classroom.status = ClassRoom.STATUS.draft
         classroom.save()
 
         return classroom
