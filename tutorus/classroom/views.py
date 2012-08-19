ea1193b79df72c2489f101228347f4c124ec9219
Greg Reinbach <greg@reinbach.com>
2012-08-19 10:33:23 -0400

    only publish interest if it is newly created

--- tutorus/classroom/views.py
+++ tutorus/classroom/views.py
@@ -99,20 +99,21 @@ def class_activate(request, classroom):
 def class_interest(request, classroom_id):
     """Mark students interest in the class"""
     classroom = get_object_or_404(ClassRoom, pk=classroom_id)
-    interest, r = ClassRoomStudentInterest.objects.get_or_create(
+    interest, created = ClassRoomStudentInterest.objects.get_or_create(
         student=request.user,
         classroom=classroom
     )
-    # publish interest to class channel
-    pubnub = get_pubnub_connection()
-    pubnub.publish({
-        "channel": "tutor_{0}".format(classroom.tutor.pk),
-        "message": {
-            "type": "interest",
-            "classroom": classroom.pk,
-            "interest": classroom.interest(),
-        }
-    })
+    # publish interest to class channel, only if interest added
+    if created:
+        pubnub = get_pubnub_connection()
+        pubnub.publish({
+            "channel": "tutor_{0}".format(classroom.tutor.pk),
+            "message": {
+                "type": "interest",
+                "classroom": classroom.pk,
+                "interest": classroom.interest(),
+            }
+        })
     return HttpResponse(json.dumps({"success": True}))
 
 
