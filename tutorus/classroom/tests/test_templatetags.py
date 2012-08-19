b653cf26334db822a8d300f58d165255329dae1c
John Costa <john.costa@gmail.com>
2012-08-19 08:57:41 -0400

    add intial top questions template tag unit test

--- tutorus/classroom/tests/test_templatetags.py
+++ tutorus/classroom/tests/test_templatetags.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import User
 from django.test.testcases import TestCase
 
 from classroom.models import ClassRoom
-from classroom.templatetags.classroom_tags import latest_questions
+from classroom.templatetags import classroom_tags
 
 __docformat__ = 'restructuredtext en'
 
@@ -32,8 +32,29 @@ class WhenNoQuestionsForUser(TestCase):
         return classroom
 
     def setUp(self):
-        self.latest_questions = latest_questions(self.classroom,
-                                                 self.tutor_user)
+        self.latest_questions = classroom_tags.latest_questions(
+            self.classroom, self.tutor_user)
 
     def test_it(self):
         self.assertEqual(len(self.latest_questions['questions']), 0)
+
+
+class WhenNoTopQuestions(TestCase):
+
+    @property
+    def tutor_user(self):
+        return User.objects.get_or_create(username='tutor_user')[0]
+
+    @property
+    def classroom(self):
+        classroom = ClassRoom(name='My classroom',
+                              tutor=self.tutor_user,
+                              description='My classroom description')
+        classroom.save()
+        return classroom
+
+    def setUp(self):
+        self.top_questions = classroom_tags.top_questions(self.classroom)
+
+    def test_it(self):
+        self.assertEqual(len(self.top_questions['questions']), 0)
