dd455dcbcb7b4c30eff8a866a47b59adf68a2aee
Greg Reinbach <greg@reinbach.com>
2012-08-19 10:23:16 -0400

    added classes js file to handle classes channel subscriptions

---
+++ tutorus/core/static/js/classes.js
@@ -0,0 +1,34 @@
+function subscribeClassesChannel() {
+    pubnub = getPubNubConn();
+    pubnub.subscribe({
+        channel: "classes",
+        restore: false,
+
+        callback: function(message) {
+            // determine which type of message
+            // and handle accordingly
+            switch (message.type) {
+            case "interest":
+                setClassroomInterest();
+                break;
+            default:
+                console.log(message.type);
+                break;
+            }
+        },
+        disconnect: function() {
+            $('#conn_status').html('<b>Closed</b>');
+	    $('#conn_status').attr("class", "label label-warning");
+        },
+
+        reconnect: function() {
+            $('#conn_status').html('<b>Connected</b>');
+	    $('#conn_status').attr("class", "label label-success");
+        },
+
+        connect: function() {
+            $('#conn_status').html('<b>Connected</b>');
+	    $('#conn_status').attr("class", "label label-success");
+        }
+    });
+}
