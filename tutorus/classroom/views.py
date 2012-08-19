fd3a06d693c7aeb7dcb4456820d9bc1c994a292a
Greg Reinbach <greg@reinbach.com>
2012-08-19 19:05:27 -0400

    Student view now updated with each step

--- tutorus/classroom/views.py
+++ tutorus/classroom/views.py
@@ -164,6 +164,7 @@ def class_take(request, classroom_id):
         user=request.user,
         scratchpad_form=scratchpad_form,
         latest_questions_count=settings.LATEST_QUESTIONS_COUNT,
+        first_step=classroom.steps[0],
     )
     if classroom.is_tutor(request.user):
         context.update(is_tutor=True)
@@ -201,7 +202,8 @@ def class_step(request, classroom, step_id):
     pub_message = {
         "type": "step",
         "data": {
-            "step": step.pk,
+            "pk": step.pk,
+            "name": step.name,
             "content": step.content
         }
     }
