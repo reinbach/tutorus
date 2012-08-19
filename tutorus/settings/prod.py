0852c6d77afdad2f41b63b91a332d523768b4be4
Ken Cochrane <KenCochrane@gmail.com>
2012-08-19 19:41:10 -0400

    couldn't push trying now

--- tutorus/settings/prod.py
+++ tutorus/settings/prod.py
@@ -1,13 +1,17 @@
-import dj_database_url
-
 from base import *
 
-DATABASES = {'default': dj_database_url.config(default='postgres://cynxhwmdptcgih:1Rhc-q2Wy7kpnDuvbJQNn8j6gY@ec2-107-20-224-107.compute-1.amazonaws.com/d6cjeo6acbuta9')}
+try:
 
-STATIC_URL = "http://s3.amazonaws.com/tutorus/"
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config(default='postgres://cynxhwmdptcgih:1Rhc-q2Wy7kpnDuvbJQNn8j6gY@ec2-107-20-224-107.compute-1.amazonaws.com/d6cjeo6acbuta9')}
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'app6407022@heroku.com'
-EMAIL_HOST_PASSWORD = '0dgtbuhf'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+    STATIC_URL = "http://s3.amazonaws.com/tutorus/"
+
+    EMAIL_HOST = 'smtp.sendgrid.net'
+    EMAIL_HOST_USER = 'app6407022@heroku.com'
+    EMAIL_HOST_PASSWORD = '0dgtbuhf'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+except ImportError:
+    pass
